f0fe37177507f0752cf4732c0af3f05be1671012
0verfl0w767 <98698629+0verfl0w767@users.noreply.github.com>
2026-04-04 15:51:54 +0900

    [#1] 프로젝트 기본 구조 및 협업 환경 설정 (#2)

---
+++ app/src/main/java/com/example/day2day/presentation/common/NavigationBarInsetHelper.java
@@ -0,0 +1,29 @@
+package com.example.day2day.presentation.common;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public final class NavigationBarInsetHelper {
+    private NavigationBarInsetHelper() {
+    }
+
+    public static void applyBottomInset(View rootView, View targetView) {
+        ViewGroup.MarginLayoutParams layoutParams =
+                (ViewGroup.MarginLayoutParams) targetView.getLayoutParams();
+        final int baseBottomMargin = layoutParams.bottomMargin;
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (view, windowInsets) -> {
+            Insets navigationBars = windowInsets.getInsets(WindowInsetsCompat.Type.navigationBars());
+            ViewGroup.MarginLayoutParams params =
+                    (ViewGroup.MarginLayoutParams) targetView.getLayoutParams();
+            params.bottomMargin = baseBottomMargin + navigationBars.bottom;
+            targetView.setLayoutParams(params);
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(rootView);
+    }
+}
