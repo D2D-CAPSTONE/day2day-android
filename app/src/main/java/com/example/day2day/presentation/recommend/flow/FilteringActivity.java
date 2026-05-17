0812943d2445c735f84eb28ae2581e36723a1591
tmdals_1027 <126680358+6winmin@users.noreply.github.com>
2026-05-17 15:56:37 +0900

    필터 페이지 x, 초기화 버튼 활성화 및 원하는 분위기 중복 선택 기능 추가

--- app/src/main/java/com/example/day2day/presentation/recommend/flow/FilteringActivity.java
+++ app/src/main/java/com/example/day2day/presentation/recommend/flow/FilteringActivity.java
@@ -2,9 +2,14 @@ package com.example.day2day.presentation.recommend.flow;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.day2day.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class FilteringActivity extends AppCompatActivity {
   @Override
@@ -12,7 +17,24 @@ public class FilteringActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_filtering);
 
-    Button nextButton = findViewById(R.id.btn_filtering_next);
+    TextView tvClose = findViewById(R.id.tv_filter_close);
+    LinearLayout llReset = findViewById(R.id.ll_filter_reset);
+    ChipGroup chipGroupMood = findViewById(R.id.chip_group_mood);
+    RadioGroup rgSort = findViewById(R.id.rg_sort);
+    View nextButton = findViewById(R.id.btn_filtering_next);
+
+    tvClose.setOnClickListener(v -> finish());
+
+    llReset.setOnClickListener(
+        v -> {
+          for (int i = 0; i < chipGroupMood.getChildCount(); i++) {
+            View child = chipGroupMood.getChildAt(i);
+            if (child instanceof Chip) {
+              ((Chip) child).setChecked(false);
+            }
+          }
+          rgSort.check(R.id.rb_recommended);
+        });
 
     nextButton.setOnClickListener(
         v -> startActivity(new Intent(FilteringActivity.this, CourseMapPageActivity.class)));
